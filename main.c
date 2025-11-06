8144f7022dc4e7da04111fa154d834b0ae70199e
wngydls-lang <wngydls@sookmyung.ac.kr>
2025-11-06 15:25:00 +0900

    실습4

--- main.c
+++ main.c
@@ -3,12 +3,10 @@
 #include <string.h>
 
 void main(void){
-     char src[] = "The worst things to eat before you sleep";
-     char dst[100];
+     char str[30] = "happy C programming";
      
-     strcpy(dst, src);
+     printf("문자열 \"%s\"의 길이 : %i\n", str, strlen(str)); 
      
-     printf("copied string : %s\n", dst);
      system("PAUSE");
      }
 
